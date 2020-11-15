e0e6338c6ea757fad2c41e9e19d60e0e9764b179
salza323 <sal.zamora480@gmail.com>
2020-11-15 16:01:41 -0700

    tables built out, user can login & register

--- api/server.js
+++ api/server.js
@@ -1,14 +1,27 @@
-const express = require('./node_modules/express');
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 
+//3 TODO uncomment once 1 & 2 TODO done
+const authenticate = require('../auth/restricted-middleware');
 
 const AuthRouter = require('../auth/auth-router');
-const 'ROUTER NAME HERE' = require('./node_modules/ROUTER NAME LOCATION HERE');
 
+//1 TODO, add correct route once potluck router created
+// const 'ROUTER NAME HERE' = require('./node_modules/ROUTER NAME LOCATION HERE');
 
 const server = express();
+
+server.use(helmet());
+server.use(cors());
 server.use(express.json());
 
 server.use('/auth', AuthRouter);
-server.use('/api/'ENDPOINT NAME HERE', 'ROUTER NAME HERE');
+//2 TODO, add router name and path once ptluck router created ALSO RESTRICTED!!!
+// server.use('/api/'ENDPOINT NAME HERE', 'ROUTER NAME HERE');
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'up' });
+});
 
 module.exports = server;
--- data/migrations/20201115124236_create-tables.js
+++ data/migrations/20201115124236_create-tables.js
@@ -1,1 +1,61 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('users', (tbl) => {
+      tbl.increments();
+      tbl.string('username', 128).notNullable().unique().index();
+      tbl.string('password', 128).notNullable();
+    })
 
+    .createTable('potlucks', (tbl) => {
+      tbl.increments();
+      tbl.string('potluck_name', 128).notNullable();
+      tbl
+        .integer('potluck_organizer')
+        .unsigned()
+        .references('users.id')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+    })
+
+    .createTable('potluck_guests', (tbl) => {
+      tbl.increments();
+      tbl
+        .integer('guest_id')
+        .unsigned()
+        .references('users.id')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+      tbl
+        .integer('potluck_id')
+        .unsigned()
+        .references('potlucks.id')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+    })
+
+    .createTable('food_items', (tbl) => {
+      tbl.increments();
+      tbl.string('food_name').notNullable();
+      tbl.boolean('selected?').defaultTo(0);
+      tbl
+        .integer('selected_by')
+        .unsigned()
+        .references('users.id')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+      tbl
+        .integer('potluck')
+        .unsigned()
+        .references('potlucks.id')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('food_items')
+    .dropTableIfExists('potluck_guests')
+    .dropTableIfExists('potlucks')
+    .dropTableIfExists('users');
+};
