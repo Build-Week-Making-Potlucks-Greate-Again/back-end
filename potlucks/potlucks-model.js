35d2177e76ef6303a4d62836537e5a1f31950ee3
salza323 <sal.zamora480@gmail.com>
2020-11-18 14:44:27 -0700

    can now get a potluck!

--- potlucks/potlucks-model.js
+++ potlucks/potlucks-model.js
@@ -2,9 +2,9 @@ const db = require('../data/db-config');
 
 module.exports = {
   createPotluckWithGuestsAndFoodItems,
-  find,
-  findBy,
-  findById,
+  // find,
+  // findBy,
+  // findById,
   getAPotluck,
   getAllPotlucks,
   getallFoodsForAPotluck,
@@ -47,37 +47,27 @@ async function createPotluckWithGuestsAndFoodItems(
   createFoodList(foodItems, id);
   //now that we have a potluck we can associate any number of guest ids in this potluck by creating guest id, potluck id associations
   createGuestList(guestList, id);
-  return newPotluck;
+  return { newPotluck, guestList, foodItems };
 }
 
-function find() {
-  return db('potlucks').select('id', 'potluck_name').orderBy('id');
-}
-
-function findBy(filter) {
-  return db('potlucks').where(filter).orderBy('id');
-}
-
-function findById(id) {
-  return db('potlucks').where({ id }).first();
-}
+//----------------------------------------------------------------
 
 //this gets all foods that pertain to a specific potluck
-function getallFoodsForAPotluck(id) {
-  return (
-    db('food_items as f')
-      .join('potlucks as p, f.potluck, p.id')
-      // .select('f.food_name')
-      .where({ 'f.potluck': id })
-  );
+async function getallFoodsForAPotluck(id) {
+  return db('food_items as f').select('f.food_name').where({ 'f.potluck': id });
 }
 
-//this gets a potluck with all foods that pertain to it
+// this gets all users that pertain to a specific potluck
+async function getAllGuestsForAPotluck(id) {
+  return db('potluck_guests as pg')
+    .join('users as u', 'pg.guest_id', 'u.id')
+    .select('u.username')
+    .where({ 'pg.potluck_id': id });
+}
+//this gets a potluck with all foods and guests that pertain to it
 async function getAPotluck(id) {
   const potluckInfo = await db('potlucks as p')
     .join('users as u', 'p.potluck_organizer', 'u.id')
-    // .join('food_items as f', 'f.potluck', 'p.id')
-    // .join('potluck_guests as pg', 'pg.potluck_id', 'p.id')
     .select(
       'u.username as organizer',
       'p.potluck_name',
@@ -85,15 +75,16 @@ async function getAPotluck(id) {
       'p.date',
       'p.time',
       'p.location'
-      // 'f.food_name',
-      // 'pg.guest_id'
     )
     .where({ 'p.id': id });
   const foodInfo = await getallFoodsForAPotluck(id);
+  const guestsInfo = await getAllGuestsForAPotluck(id);
   console.log('food info:', foodInfo);
   console.log('potluck info:', potluckInfo);
-  return { potluckInfo, foodInfo };
+  return { potluckInfo, foodInfo, guestsInfo };
 }
+
+//------------------------------------------------------------------
 
 //this gets all of the polucks in the db
 function getAllPotlucks() {
@@ -112,3 +103,15 @@ function getAllPotlucks() {
       'pg.guest_id'
     );
 }
+
+// function find() {
+//   return db('potlucks').select('id', 'potluck_name').orderBy('id');
+// }
+
+// function findBy(filter) {
+//   return db('potlucks').where(filter).orderBy('id');
+// }
+
+// function findById(id) {
+//   return db('potlucks').where({ id }).first();
+// }
